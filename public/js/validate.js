5c577ea53b395528ba4d76efdb0f47cba379cddc
hyperac1d <dlastgamer18@gmail.com>
2017-08-18 14:12:40 +0800

    html5-localStorage

--- public/js/validate.js
+++ public/js/validate.js
@@ -3,27 +3,21 @@ function isLogin(){
         let input_password= document.querySelector('#password').value;
         let account = JSON.parse(localStorage.getItem("json_names"));
         let html=``;
+        let result = false
 
         console.log(account);
+        console.log("length of json: "+account.length)
 
         try{
             for(var i = 0; i < account.length; i++){
-                if(account == null){
-                    alert("Invalid Username or Password");
+                if(account[i].username == input_username && account[i].password == input_password){
+                    result = true
                 }
                 
-                else if(account[i].username == input_username && account[i].password == input_password){
-                    console.log(account[i].username);
-                    console.log(account[i].password);
-                    window.open("/list");
-                    break;
-                }
-
-                else{
-                    alert("Invalid Username or Password");
-                    break;
-                }
-                
+            }
+            if(result){
+                alert("Welcome to Echo!")
+                window.open("/list")
             }
         }
 
